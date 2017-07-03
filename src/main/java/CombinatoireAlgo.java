bab1ac12e138976ee8141c1b71c76b82952de21f
Romain LOUVET <romain.louvet@schibsted.com>
2017-07-03 10:58:59 +0200

    first commit

---
+++ src/main/java/CombinatoireAlgo.java
@@ -0,0 +1,16 @@
+/**
+ * Created by vaik on 29/06/17.
+ */
+
+/*
+
+coding exercise: create a class that computes all permutations of a string.
+Eg “abc” returns “abc”, “acb”, “bac”, “bca”, “cab” and “cba”.
+It should work for any size of string (3, 4 , 5 and more...)
+
+You can make as many public/private fonctions you want to help you.
+
+ */
+
+public class CombinatoireAlgo {
+}
---
+++ src/test/java/CombinatoireAlgoTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+/**
+ * Created by vaik on 29/06/17.
+ */
+
+/*
+  Make some tests to verify :
+  - there is not the same result twice
+  - the expected number of results is correct
+  - anything you think it's relevant
+ */
+
+public class CombinatoireAlgoTest {
+}
